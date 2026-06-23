97d8975b4d880269abfe45945cb6aef6cb095767
rohinaruna-dev <rohinaruna@gmail.com>
2026-06-23 14:35:27 +0530

    wf se

--- software-engineering-core-concepts/vite.config.ts
+++ software-engineering-core-concepts/vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/software-engg/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
